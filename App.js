c85a341454fc29d927e0d53a56304b586c5a6ded
DavidMes17 <davidmes46@gmail.com>
2024-03-26 02:10:25 -0300

    navegação entre telas

--- App.js
+++ App.js
@@ -14,17 +14,38 @@ import { Topo } from './src/components/topo';
 import { Botoes } from './src/components/botoes';
 import { RelatorioRenda } from './src/components/relatorioRenda';
 
+import Cobranca from './src/components/screens/cobranca';
+import Imprevisto from './src/components/screens/imprevisto';
+import Salario from './src/components/screens/salario';
+import Outro from './src/components/screens/outro';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
 export default function App() {
 
   return (
-    <View style={styles.container}>
-
-      <Topo />
-      <Botoes />
-      <RelatorioRenda />
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#674FFF' }, headerTintColor: '#fff' }} >
+        <Stack.Screen name='index' component={Index} options={{ headerShown: false }} />
+        <Stack.Screen name='COBRANÇA' component={Cobranca} />
+        <Stack.Screen name='IMPREVISTO' component={Imprevisto} />
+        <Stack.Screen name='SALÁRIO' component={Salario} />
+        <Stack.Screen name='OUTRO' component={Outro} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+function Index({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Topo />
+      <Botoes navigation={navigation} />
+      <RelatorioRenda />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
--- src/components/botoes.jsx
+++ src/components/botoes.jsx
@@ -3,32 +3,48 @@ import {
 } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-function Botoes() {
+function Botoes({ navigation }) {
     return (
         <View style={styles.botao}>
             <View style={styles.botaoChildren}>
-                <TouchableOpacity style={styles.inputBtn}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('COBRANÇA')
+                    }}
+                    style={styles.inputBtn}>
                     <Ionicons style={styles.icon} name="newspaper-outline"></Ionicons>
                 </TouchableOpacity>
                 <Text>COBRANÇA</Text>
             </View>
 
             <View style={styles.botaoChildren}>
-                <TouchableOpacity style={styles.inputBtn}>
+                <TouchableOpacity
+                onPress={()=>{
+                    navigation.navigate('IMPREVISTO')
+                }}
+                style={styles.inputBtn}>
                     <Ionicons style={styles.icon} name="document-text-outline"></Ionicons>
                 </TouchableOpacity>
                 <Text>IMPREVISTO</Text>
             </View>
 
             <View style={styles.botaoChildren}>
-                <TouchableOpacity style={styles.inputBtn}>
+                <TouchableOpacity
+                onPress={()=>{
+                    navigation.navigate('SALÁRIO')
+                }}
+                style={styles.inputBtn}>
                     <Ionicons style={styles.icon} name="wallet-outline"></Ionicons>
                 </TouchableOpacity>
                 <Text>SALÁRIO</Text>
             </View>
 
             <View style={styles.botaoChildren}>
-                <TouchableOpacity style={styles.inputBtn}>
+                <TouchableOpacity
+                onPress={()=>{
+                    navigation.navigate('OUTRO')
+                }}
+                style={styles.inputBtn}>
                     <Ionicons style={styles.icon} name="ellipsis-horizontal-circle-outline"></Ionicons>
                 </TouchableOpacity>
                 <Text>OUTRO</Text>
