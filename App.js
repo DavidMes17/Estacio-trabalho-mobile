4b1b3c06ecfea671a6837e0a940fbc110b118f28
DavidMes17 <davidmes46@gmail.com>
2024-03-28 21:20:48 -0300

    requisição da biometria ao entrar (android)

--- App.js
+++ App.js
@@ -7,9 +7,13 @@
   #FFE84F
   #D94A3B
 */}
+import { useEffect, useState } from 'react';
 import {
-  StyleSheet, View
+  Alert,
+  Button,
+  StyleSheet, Text, View
 } from 'react-native';
+import * as localAuthentication from 'expo-local-authentication'
 import { Topo } from './src/components/topo';
 import { Botoes } from './src/components/botoes';
 import { RelatorioRenda } from './src/components/relatorioRenda';
@@ -28,6 +32,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#674FFF' }, headerTintColor: '#fff' }} >
+        <Stack.Screen name='auth' component={Biometria} options={{ headerShown: false }} />
         <Stack.Screen name='index' component={Index} options={{ headerShown: false }} />
         <Stack.Screen name='COBRANÇA' component={Cobranca} />
         <Stack.Screen name='IMPREVISTO' component={Imprevisto} />
@@ -37,6 +42,45 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+function Biometria({ navigation }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  async function Autenticacao() {
+    const compativel = await localAuthentication.hasHardwareAsync()
+    console.log('compatível com biometria: ' + compativel)
+
+    const tiposAutenticacao = await localAuthentication.supportedAuthenticationTypesAsync()
+    console.log('tipos de biometria disponíveis: ' + tiposAutenticacao)
+
+  }
+  async function PedirBiometria() {
+    const temBiometria = await localAuthentication.isEnrolledAsync()
+    console.log('biometria cadastrada: ' + temBiometria)
+    if (!temBiometria) {
+      return Alert.alert('Aviso', 'Não há biometria cadastrada')
+    }
+
+    const auth = await localAuthentication.authenticateAsync({
+      promptMessage: 'Biometria',
+      fallbackLabel: 'Erro na biometria'
+    })
+
+    if (auth.success) {
+      setIsAuthenticated(true)
+      navigation.replace('index')
+    }
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Acesse Seu App com a biometria</Text>
+      <Button title='Pedir biometria' onPress={PedirBiometria} />
+    </View>
+  );
+}
+
 
 function Index({ navigation }) {
   return (
